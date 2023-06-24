ac14d721411a5d255a9851b4461903a98f63682f
Peter Chapman <petechapman@protonmail.com>
2023-06-24 13:53:38 -0400

    fix: add . to m4b

--- librarian/constants.ts
+++ librarian/constants.ts
@@ -1,3 +1,3 @@
-export const AUDIOBOOK_EXTENSIONS = ['.mp3', '.mp4', 'm4b'];
+export const AUDIOBOOK_EXTENSIONS = ['.mp3', '.mp4', '.m4b'];
 export const COMIC_BOOK_EXTENSIONS = ['.cbz', '.cbr'];
 export const EBOOK_EXTENSIONS = ['.mobi', '.epub', ".pdf"];
