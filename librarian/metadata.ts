5dab6b882af2f73119572d764a362fddb4b9fd19
Peter Chapman <petechapman@protonmail.com>
2023-06-24 16:49:10 -0400

    chore: testing isbn

--- librarian/metadata.ts
+++ librarian/metadata.ts
@@ -20,7 +20,7 @@ export const formatISBN = (isbn: number | string): string => typeof isbn === 'st
       : String(isbn);
 
 export const isISBN = (isbn: string): boolean => {
-  console.log('TESTING ISBN: isbn')
+  console.log('TESTING ISBN: ', isbn)
   const isbn10Regex = /^(?:[0-9]{9}X|[0-9]{10})$/; // ISBN-10 should be 9 digits followed by an X or 10 digits
   const isbn13Regex = /^(?:97[89][0-9]{10})$/; // ISBN-13 should start with 978 or 979 followed by 10 digits
   const iValidISBN = isbn10Regex.test(isbn) || isbn13Regex.test(isbn);
