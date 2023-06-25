e15a5c340e4669322aeb5388c53b95164f01af5d
Peter Chapman <petechapman@protonmail.com>
2023-06-25 17:54:57 -0400

    chore: omit key

--- librarian/services.ts
+++ librarian/services.ts
@@ -34,8 +34,7 @@ export async function fetchGoogleBookISBN(title: string, author: string, publish
 }
 
 export const fetchGoogleBooksMetadata = async (isbn: string) => {
-  const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
-  const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
 
   const response = await axios.get(url);
   const data = response.data;
